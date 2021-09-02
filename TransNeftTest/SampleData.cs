344c4043c17cf4915f1a8660f60f250b552d6c81
Vladimir Lapin <lovember@ya.ru>
2021-09-03 02:17:23 +0300

    SampleData created and started to be filled.

--- TransNeftTest/SampleData.cs
+++ TransNeftTest/SampleData.cs
@@ -12,10 +12,46 @@ namespace TransNeftTest
         {
             if (!context.Holdings.Any())
             {
-                context.Holdings.Add(new Holding { Name = "Транснефть" });
+                context.Holdings.Add(
+                    new Holding
+                    {
+                        Name = "Транснефть",
+                        Address = "123112, г. Москва, Пресненская набережная, д. 4, стр. 2"
+                    });
             }
 
+            if (!context.Subsidiaries.Any())
+            {
+                context.Subsidiaries.Add(
+                    new Subsidiary
+                    {
+                        Name = "ТранснефтьЭнерго",
+                        Address = "123112, Москва, Пресненская наб., д.4, стр.2, башня \"Эволюция\"",
+                        HoldingId = 1
+                    });
+            }
 
+            if (!context.Consumers.Any())
+            {
+                context.Consumers.AddRange(
+                    new Consumer
+                    {
+                        Name = "ПС 110/10 Весна",
+                        Address = "В деревне у дедушки",
+                        SubsidiaryId = 1
+                    },
+                    new Consumer
+                    {
+                        Name = "Потребитель-1",
+                        Address = "Адрес потребителя-1",
+                        SubsidiaryId = 1
+                    });
+            }
+
+            if (!context.MeasurePoints.Any())
+            {
+
+            }
         }
     }
 }
